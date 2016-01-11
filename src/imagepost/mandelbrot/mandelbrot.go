adf7fd5fed329aaef6e70ca78e3aa4a577540845
Ronny Brzezinski <rbrzezinski@clypd.com>
2016-01-11 15:08:42 -0500

    add code to draw the Mandelbrot set
    Uses image library to set colors of pixels based on whether the point
    is in the Mandelbrot set, or how 'close' it is to being in the Mandelbrot
    set, 'close' meaning how many iterations it took to determine if it isn't
    in it.

--- src/imagepost/mandelbrot/mandelbrot.go
+++ src/imagepost/mandelbrot/mandelbrot.go
@@ -1,8 +1,24 @@
 package mandelbrot
 
 import (
+	"image"
+	"image/color"
 	"math/cmplx"
 )
+
+// View is what you are looking at, the rectangle
+// within these two complex numbers in the plane
+type Range struct {
+	Max  complex128
+	Min  complex128
+	Iter int
+}
+
+// width and height of picture
+type Frame struct {
+	Width  int
+	Height int
+}
 
 // inMandelbrotSet is the logic of the mandelbrot set. This checks if a complex
 // number is in the mandelbrot set or not, and if it bails returns how many
@@ -18,5 +34,55 @@ func inMandelbrotSet(n complex128, maxIteration int) (bool, int) {
 		return true, 0
 	}
 	return false, i
+
+}
+
+// Converts pixel at x,y coordinate to corresponding complex number
+func pixelToCmplx(x, y int, r Range, f Frame) complex128 {
+	minX := real(r.Min)
+	minY := imag(r.Min)
+
+	maxX := real(r.Max)
+	maxY := imag(r.Max)
+
+	pixelWidth := (maxX - minX) / float64(f.Width)
+	pixelHeight := (maxY - minY) / float64(f.Height)
+
+	resultX := float64(x)*pixelWidth + minX
+	resultY := maxY - float64(y)*pixelHeight //how pixel coordinates are
+
+	return complex(resultX, resultY)
+}
+
+// Creates a JPEG mandelbrot image
+func MandelImage(r Range, f Frame) image.Image {
+	m := image.NewRGBA(image.Rect(0, 0, f.Width, f.Height))
+	ch := make(chan struct{}, 4)
+	for x := 0; x < f.Width; x++ {
+		go setMandelImageCol(x, m, ch, r, f)
+	}
+	for count := 0; count < f.Width; count++ {
+		<-ch
+	}
+	return m
+}
+
+// helper function to mandelImage function. Sets all coordinates in
+// given image in column x
+func setMandelImageCol(x int, m *image.RGBA, ch chan struct{}, r Range, f Frame) {
+	maxIteration := r.Iter
+	for y := 0; y < f.Height; y++ {
+		num := pixelToCmplx(x, y, r, f)
+		inSet, i := inMandelbrotSet(num, maxIteration)
+		if inSet {
+			m.Set(x, y, color.Black)
+		} else {
+			bComp := i % 255
+			rComp := 255 - bComp
+			colr := color.RGBA{R: uint8(rComp), G: 0, B: uint8(bComp), A: 0}
+			m.Set(x, y, colr)
+		}
+	}
+	ch <- struct{}{}
 
 }
